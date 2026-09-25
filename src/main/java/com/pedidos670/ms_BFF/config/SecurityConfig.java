23c26ef7af9b6043852c7e93364f9e58725f2be1
Elias Casa <eliasaaronalfaro@gmail.com>
2026-09-25 15:52:21 -0300

    permitir cors

--- src/main/java/com/pedidos670/ms_BFF/config/SecurityConfig.java
+++ src/main/java/com/pedidos670/ms_BFF/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -25,15 +26,20 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                         .requestMatchers(
                                 HttpMethod.GET,
                                 "/api/bff/productos/**"
                         ).permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
